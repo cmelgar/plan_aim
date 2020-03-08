bc0b3b103d27e7c64158df3b3902bd1093c3eae5
carlos <carlos@ubuntu.ubuntu-domain>
2020-03-08 00:49:13 -0500

    implementing add feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import connection
 
 app = Flask(__name__)
@@ -13,6 +13,20 @@ data = cur.fetchall()
 @app.route('/')
 def home():
     return render_template('index.html', data=data)
+
+@app.route('/get/<int:id>')
+def get_aim(id):
+    conn = connection.create_connection("aimDB")
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM aimTB WHERE id =?", (id ,))
+    aim = cur.fetchall()
+    print("This is my id: ", id)
+    print("This is the aim: ", aim)
+    return render_template('index.html', data=data, aim=aim)
+
+@app.route('/add')
+def add_aim():
+    return render_template("add_aim.html", data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
